4300a6f7a31d0a03d7af6b4a948c4b13aed6fbcb
gabrieljakubek <gabrieljakubek@gmail.com>
2019-03-25 00:37:25 -0300

    Ejercicio 7 casi terminado

--- Ejercicios/Jakubek.Gabriel.2.D.Ejercicio/Ejercicio07/Program.cs
+++ Ejercicios/Jakubek.Gabriel.2.D.Ejercicio/Ejercicio07/Program.cs
@@ -12,8 +12,9 @@ namespace Ejercicio07
         static bool auxBool;
         static bool anoBisiesto = false;
         static int diaNacimiento;
-        static int mesNAcimiento;
+        static int mesNacimiento;
         static int anoNacimiento;
+        static int diasTotales;
         static void Main(string[] args)
         {
             Console.Title = "Ejercicio 06";
@@ -32,9 +33,63 @@ namespace Ejercicio07
                     {
                         anoBisiesto = true;
                     }
-                        auxBool = true;
+                    auxBool = true;
                 }
             } while (!auxBool);
+            auxBool = false;
+            do
+            {
+                Console.Write("Ingrese su mes de nacimiento: ");
+                int.TryParse(Console.ReadLine(), out ingreso);
+                if (ingreso <= 0 || ingreso > 12)
+                {
+                    Console.WriteLine("Error. Reingresar el mes!");
+                }
+                else
+                {
+                    mesNacimiento = ingreso;
+                    auxBool = true;
+                }
+            } while (!auxBool);
+            auxBool = false;
+            do
+            {
+                Console.Write("Ingrese su dia de nacimiento: ");
+                int.TryParse(Console.ReadLine(), out ingreso);
+                if (ingreso <= 0 || (mesNacimiento == 1 || mesNacimiento == 3 || mesNacimiento == 5
+                    || mesNacimiento == 7 || mesNacimiento == 8 || mesNacimiento == 10
+                    || mesNacimiento == 12) && ingreso > 31
+                    || mesNacimiento == 2 && anoBisiesto == false && ingreso > 28
+                    || mesNacimiento == 2 && anoBisiesto == true && ingreso > 29
+                    || (mesNacimiento == 4 || mesNacimiento == 6 || mesNacimiento == 9
+                    || mesNacimiento == 11) && ingreso > 30)
+                {
+                    Console.WriteLine("Error. Reingresar el dia!");
+                }
+                else
+                {
+                    diaNacimiento = ingreso;
+                    auxBool = true;
+                }
+            } while (!auxBool);
+            for (int i = mesNacimiento; i <= 12; i++)
+            {
+                diasTotales += DateTime.DaysInMonth(anoNacimiento, i);
+            }
+            diasTotales -= diaNacimiento;
+            for (int j = anoNacimiento + 1; j < DateTime.Now.Year; j++)
+            {
+                for (int k = 1; k <= 12; k++)
+                {
+                    diasTotales += DateTime.DaysInMonth(j, k);
+                }
+            }
+            for (int h = 1; h <= DateTime.Now.Month; h++)
+            {
+                diasTotales += DateTime.DaysInMonth(DateTime.Now.Year, h);
+            }
+            Console.Write("Usted a vivido {0} dias", diasTotales);
+            Console.ReadKey();
         }
     }
 }
