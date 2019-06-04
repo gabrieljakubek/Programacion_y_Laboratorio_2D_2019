b7d14b64948d11f608e3ee3a7258e553ceaccd67
gabrieljakubek <gabrieljakubek@gmail.com>
2019-06-04 01:00:29 -0300

    Practica de SQL

---
+++ Practica/SQL/EjercicioSQLWF/EjercicioSQLWF/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace EjercicioSQLWF
+{
+    public partial class Form1 : Form
+    {
+        List<Provincias> provincias;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            provincias = new List<Provincias>();
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.MiCadenaSQL);
+            SqlCommand comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.Connection = conexion;
+            comando.CommandText = "SELECT Id, Nombre FROM Provincias";
+            conexion.Open();
+            SqlDataReader reader = comando.ExecuteReader();
+            while (reader.Read())
+            {
+                provincias.Add(new Provincias((int)((decimal)reader["Id"]) ,reader["Nombre"].ToString()));
+            }
+            this.cboProvincia.DataSource = provincias;
+            conexion.Close();
+            //MessageBox.Show(Properties.Settings.Default.MiCadenaSQL);
+        }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            SqlConnection conexion = new SqlConnection(Properties.Settings.Default.MiCadenaSQL);
+            SqlCommand comando = new SqlCommand();
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.Connection = conexion;
+            string consulta = "INSERT INTO Localidades (ProvinciaId, Nombre) Values('"+((Provincias)cboProvincia.SelectedItem).Id+"','"+ txtLocalidad.Text + "')";
+            comando.CommandText = consulta;
+            conexion.Open();
+            comando.ExecuteNonQuery();
+            conexion.Close();
+        }
+    }
+}
---
+++ Practica/SQL/EjercicioSQLWF/EjercicioSQLWF/Provincias.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjercicioSQLWF
+{
+    public class Provincias
+    {
+        public string nombre;
+        public int id;
+
+        public int Id { get {return this.id; } set {this.id = value; } }
+        public string Nombre
+        {
+            get
+            {
+                return this.nombre;
+            }
+            set
+            {
+                this.nombre = value;
+            }
+        }
+
+        public Provincias(int id,string nombre)
+        {
+            this.id = id;
+            this.nombre = nombre;
+        }
+
+        public override string ToString()
+        {
+            return this.nombre;
+        }
+    }
+}
